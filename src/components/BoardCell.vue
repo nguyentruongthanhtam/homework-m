d55f55bb770fe104be8a9caf98a91fd4d2cb0676
Tam Nguyen <tam.nguyen.300390@gmail.com>
2024-02-17 15:10:34 +0200

    Add cell component

---
+++ src/components/BoardCell.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps(['content'])
+
+console.log(props.content)
+</script>
+<template>
+  <div class="cell">{{ content }}</div>
+</template>
+
+<style scoped>
+.cell {
+  border: 1px solid red;
+  width: calc(100% / 7);
+  height: calc(100% / 9);
+}
+@media (min-width: 1024px) {
+}
+</style>
