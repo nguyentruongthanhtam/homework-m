33f29961bb86f8877af51fdc8ef7a14722e8d6e3
Tam Nguyen <tam.nguyen.300390@gmail.com>
2024-02-17 15:10:26 +0200

    Add board component

---
+++ src/components/MergeBoard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import BoardCell from './BoardCell.vue'
+</script>
+
+<template>
+  <section class="wrapper">
+    <div class="board">
+      <BoardCell v-for="i in 63" :content="i"/>
+    </div>
+  </section>
+</template>
+
+<style>
+.wrapper {
+  width: 80vw;
+  height: 80vh;
+  padding: 1em;
+  .board {
+    width: 100%;
+    height: 100%;
+    border: 1px solid #333;
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+</style>
