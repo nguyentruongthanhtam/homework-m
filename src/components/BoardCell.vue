5f318deb382ee0d95cf0143c34fc82855278af46
Tam Nguyen <tam.nguyen.300390@gmail.com>
2024-02-17 17:59:51 +0200

    Add slot, dynamic grid, select cell event

--- src/components/BoardCell.vue
+++ src/components/BoardCell.vue
@@ -1,17 +1,28 @@
 <script lang="ts" setup>
-const props = defineProps(['content'])
-
-console.log(props.content)
+import { ref, toRef } from 'vue'
+const props = defineProps(['data', 'isActive'])
+const css = props.data.css
+const isActive = toRef(props, 'isActive')
+const cssProps = {
+  '--board-width': css.width,
+  '--board-height': css.height
+}
 </script>
 <template>
-  <div class="cell">{{ content }}</div>
+  <div class="cell" :style="cssProps" :class="{ active: isActive }" @click="$emit('onActive')">
+    <slot name="image"></slot>
+  </div>
 </template>
 
 <style scoped>
 .cell {
-  border: 1px solid red;
-  width: calc(100% / 7);
-  height: calc(100% / 9);
+  width: calc(100% / var(--board-width));
+  height: calc(100% / var(--board-height));
+  word-break: break-all;
+  user-select: none;
+  &.active {
+    border: 1px solid red;
+  }
 }
 @media (min-width: 1024px) {
 }
--- src/components/MergeBoard.vue
+++ src/components/MergeBoard.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import BoardCell from './BoardCell.vue'
+import payload from '../assets/assigment.json'
+console.log(payload)
+const boardSize = payload.width * payload.height
+const items = payload.items
+const chosenCell = ref(-1)
+const cellProps = {
+  css: {
+    width: payload.width,
+    height: payload.height
+  }
+}
+
+function setChosenCell(cellIndex: number) {
+  console.log(cellIndex)
+
+  chosenCell.value = cellIndex
+}
 </script>
 
 <template>
   <section class="wrapper">
     <div class="board">
-      <BoardCell v-for="i in 63" :content="i"/>
+      <BoardCell
+        v-for="(n, index) in boardSize"
+        :key="index"
+        :data="cellProps"
+        :isActive="index === chosenCell"
+        @on-active="setChosenCell(index)"
+      >
+        <template #image>
+          <div>{{ items[index]?.itemType }}</div>
+        </template>
+      </BoardCell>
     </div>
   </section>
 </template>
