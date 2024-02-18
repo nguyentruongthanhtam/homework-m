a7d3433a9d5e7d7cfe19eb03301a2d9e84cb2758
Tam Nguyen <tam.nguyen.300390@gmail.com>
2024-02-18 21:38:19 +0200

    Adjust responsive layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,23 +5,37 @@ import RightPanel from '@/components/RightPanel.vue'
 
 <template>
   <main>
-    <section class="board">
-      <MergeBoard />
-    </section>
-    <aside class="panel">
+    <aside class="panel-container">
       <RightPanel />
     </aside>
+    <section class="board-container">
+      <MergeBoard />
+    </section>
   </main>
 </template>
 
 <style>
 main {
   height: calc(100% - 50px);
-  .board {
-    height: 80%;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  .panel-container {
+    height: 25%;
   }
-  .panel {
-    height: 20%;
+  .board-container {
+    height: 70%;
+  }
+}
+@media (min-width: 1024px) {
+  main {
+    .panel-container {
+      height: 20%;
+    }
+    .board-container {
+      height: 75%;
+    }
   }
 }
 </style>
