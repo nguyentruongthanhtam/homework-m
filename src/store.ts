729aaca4d0e6d8c41c2c1e9f062c6d3abc4b1764
Tam Nguyen <tam.nguyen.300390@gmail.com>
2024-02-18 20:00:26 +0200

    Add types and mutation to store

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,32 @@
 // store.js
-import { reactive } from 'vue'
+import {ref, reactive } from 'vue'
+import jsonData  from './assets/assigment.json'
+export interface JsonData {
+  boardId: string
+  width: number
+  height: number
+  items: (Item | null)[]
+}
 
+export interface Item {
+  itemId: number
+  itemType: string
+  chainId: string
+  pausedUntil: string|null
+  createdAt: string
+  visibility: 'hidden' | 'visible'
+  itemLevel: number
+  isInsideBubble: boolean
+}
+
+export type ItemType = 'itemId' |'itemType' | 'chainId' | 'pausedUntil' | 'createdAt' | 'visibility' | 'itemLevel' | 'isInsideBubble'
+const data : JsonData = jsonData;
 export const store = reactive({
-  chosenCell: -1
+  chosenCell: ref(-1),
+  data,
+  updateValue(type: ItemType, index:number, newValue: string|number|boolean) {
+    if(data.items[index]) {
+      (data.items[index] as unknown as Record<string, string | number | boolean | 'hidden' | 'visible'>)[type] = newValue
+    }
+  },
 })
